6120d74bdb9416a661db26a372924bb4e741bf95
XuanBac3105 <xuanbac05@gmail.com>
2025-10-28 22:19:44 +0700

    Setup Validation & Transform

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,28 @@ import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { SharedModule } from './shared/shared.module';
+import { APP_FILTER, APP_INTERCEPTOR, APP_PIPE } from '@nestjs/core';
+import { ZodSerializerInterceptor } from 'nestjs-zod';
+import { HttpExceptionFilter } from './shared/filters/http-exception.filter';
+import CustomZodValidationPipe from './shared/pipes/custom-zod-validation.pipe';
 
 @Module({
     imports: [SharedModule],
     controllers: [AppController],
-    providers: [AppService],
+    providers: [
+        AppService,
+        {
+            provide: APP_INTERCEPTOR,
+            useClass: ZodSerializerInterceptor
+        },
+        {
+            provide: APP_FILTER,
+            useClass: HttpExceptionFilter
+        },
+        {
+            provide: APP_PIPE,
+            useClass: CustomZodValidationPipe
+        }
+    ],
 })
 export class AppModule {}
